3f4785b2772790c67fb08ccd9cc6801ac07dab05
CRR1919 <idkforlename@gmail.com>
2026-03-23 19:01:09 -0700

    2nd change
    2nd change in 1.1.3

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Stephen" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("this is cool")
 
 if __name__ == "__main__":
     main()
